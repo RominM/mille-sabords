a114b1a860a7f2c17cc1e5f77e530a71260211c6
RominM <rominmanogil.off@gmail.com>
2022-09-18 21:14:07 +0200

    feat: :construction: add looseGame function
    add condittion when you get skulls

--- millesabords/controller/looseGame.js
+++ millesabords/controller/looseGame.js
@@ -1,27 +1,20 @@
 export const looseGame = (diceInBucketlist, turn) => {
-  if (diceInBucketlist.length) {
-    let skullDice = [];
-    diceInBucketlist.forEach((dice) => {
-      skullDice.push(dice.classList.contains('skull'));
-
-      //TODO Have to check if is only skulls is verify
-      function allAreTruthy(skullDice) {
-        return skullDice.every((el) => el);
+  let skullDice = [];
+  diceInBucketlist.forEach((dice) => {
+    if (turn === 1) {
+      // you get 3 skulls, the You Loose
+      diceInBucketlist.length === 3 && console.log('YOU LOOSE !');
+      // you get more than 3 skulls, you pass into the dark side
+      // you take points to your adversers
+      diceInBucketlist.length > 3 && console.log('PASS TO THE DARK SIDE');
+    }
+    if (turn > 1) {
+      // If you get 3 skulls during the game, then You Loose
+      const skull = dice.classList.contains('skull');
+      if (skull) {
+        skullDice.push(dice);
+        skullDice.length > 2 && console.log('YOU LOOSE ?!');
       }
-      console.log(allAreTruthy(skullDice));
-
-      // if at the first turn
-      if (turn === 1) {
-        // you get 3 skulls, the You Loose
-        skullDice.length === 3 && console.log('YOU LOOSE !');
-        // you get more than 3 skulls, you pass into the dark side
-        skullDice.length > 3 && console.log('PASS TO THE DARK SIDE');
-        // you take points to your adversers
-      }
-      if (turn > 1) {
-        // If you get 3 skulls during the game, then You Loose
-        skullDice.length > 2 && console.log('YOU LOOSE !');
-      }
-    });
-  }
+    }
+  });
 };
